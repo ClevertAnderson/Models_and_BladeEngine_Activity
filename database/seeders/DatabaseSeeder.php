2156a85ff9ab883a451b259b6278192f9aba0f5a
Clevert Anderson A. Banasan <network230206@gmail.com>
2025-03-30 19:11:42 +0800

    Created Book Factory

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,6 +14,7 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         // User::factory(10)->create();
+        \App\Models\Book::factory(20)->create();
 
         User::factory()->create([
             'name' => 'Test User',
