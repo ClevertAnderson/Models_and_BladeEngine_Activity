86258c0847b33201a31a2731767a830b860adbc8
Clevert Anderson A. Banasan <network230206@gmail.com>
2025-03-31 11:39:52 +0800

    Activity Done

--- database/factories/BookFactory.php
+++ database/factories/BookFactory.php
@@ -4,16 +4,19 @@ namespace Database\Factories;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
 
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Book>
+ */
 class BookFactory extends Factory
 {
-    public function definition(): array
+    public function definition()
     {
         return [
-            'isbn' => $this->faker->isbn13,
+            'isbn' => $this->faker->isbn13(),
             'title' => $this->faker->sentence(3),
-            'author' => $this->faker->name,
-            'description' => $this->faker->paragraph,
-            'date_published' => $this->faker->date,
+            'author' => $this->faker->name(),
+            'description' => $this->faker->paragraph(),
+            'date_published' => $this->faker->date(),
         ];
     }
 }
